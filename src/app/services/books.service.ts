65cba8353a426ddb7f812cca3bce493f444fc8f0
Sahejpalarneja <sahejpalarneja@gmail.com>
2022-05-03 21:01:37 +0200

    Added a header

--- src/app/services/books.service.ts
+++ src/app/services/books.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import {Observable ,of} from 'rxjs' ;
-import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Observable } from 'rxjs' ;
+import {HttpClient} from '@angular/common/http';
 import {Book} from '../Book';
 
 
