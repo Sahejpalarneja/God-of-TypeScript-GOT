0fb27bdb6ccc7217ab44c22a52c3fa1484774bf0
Sahejpalarneja <sahejpalarneja@gmail.com>
2022-05-03 15:26:02 +0200

    Config change

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import {HttpClientModule} from '@angular/common/http'
 import { AppComponent } from './app.component';
 import { BookItemComponent } from './components/book-item/book-item.component';
 import { BooksComponent } from './components/books/books.component';
-import { CharctersComponent } from './components/charcters/charcters.component';
+import { CharctersComponent } from './components/characters/charcters.component';
 
 @NgModule({
   declarations: [
