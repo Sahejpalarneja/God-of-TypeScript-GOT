a73324833a9b2c8995c89527e3b9e38ea3e097d0
Sahejpalarneja <sahejpalarneja@gmail.com>
2022-05-09 21:13:13 +0200

    Update the houses

--- src/app/components/houses/houses.component.ts
+++ src/app/components/houses/houses.component.ts
@@ -1,18 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 import { House } from 'src/app/House';
 import { HousesService } from 'src/app/services/houses.service';
+import { Router } from '@angular/router';
+
+type PaginatorEvent = {
+  page: number;
+};
 @Component({
   selector: 'app-houses',
   templateUrl: './houses.component.html',
   styleUrls: ['./houses.component.css']
 })
 export class HousesComponent implements OnInit {
-  houses:House[] = []
+  houses:House[] = [];
 
-  constructor(private HousesService: HousesService) { }
+  private currentPage = 1;
+
+  constructor(private houseService: HousesService) { }
 
   ngOnInit(): void {
-    this.HousesService.getHouses().subscribe((houses) => (this.houses = houses))
+    this.getHouses();
+  }
+  getHouses(): void {
+    this.houseService
+      .getAllHouses(this.currentPage)
+      .subscribe((data) => (this.houses = data));
+  }
+  paginate(event: PaginatorEvent): void {
+    this.currentPage = event.page + 1;
+    this.getHouses();
+  }
+  onHouseClick(house:House){
+
   }
 
 }
--- src/app/services/houses.service.ts
+++ src/app/services/houses.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { map, Observable } from 'rxjs';
 import { HttpClient} from '@angular/common/http';
 import { House } from '../House';
 
@@ -8,10 +8,24 @@ import { House } from '../House';
 })
 export class HousesService {
   private apiUrl = 'https://anapioficeandfire.com/api/houses'
+  private pageSize = 50;
 
   constructor(private http:HttpClient) { }
 
-  getHouses(): Observable<House[]>{
+  getAllHouses(pageNumber:number): Observable<House[]>{
+    const url = `${this.apiUrl}?page=${pageNumber || 1}&pageSize=${
+      this.pageSize
+    }`;
     return this.http.get<House[]>(this.apiUrl)
+  }
+
+  getHouse(id:number){
+    const url = `${this.apiUrl}/${id}`;
+    return this.http.get<House>(url).pipe(
+      map((house, index) => {
+        const id = house.url.split('/').pop() || `${index}`;
+        return { ...house, id };
+      })
+    );
   }
 }
